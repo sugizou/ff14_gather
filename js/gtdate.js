8d9a34ee834a139312c69743b6dc7de543e13ec0
toshiyuki sugimoto <sugimoto@pentalab.net>
2015-08-26 19:05:07 +0900

    wip

---
+++ js/gtdate.js
@@ -0,0 +1,27 @@
+function GTDate() {
+  this.initialize.apply(this, arguments);
+}
+
+GTDate.prototype = {
+  baseTime: 0,
+  initialize: function() {
+    this.baseTime = (new Date('Tue, 13 Jul 2010 01:00:00 +0900')).getTime();
+  },
+  eorzeaHour: function() {
+    return parseInt(((new Date()).getTime() - this.baseTime) / 1000 / 175 % 24);
+  },
+  eorzeaMinutes: function() {
+    return parseInt(((new Date()).getTime() - this.baseTime) / 1000 / (4200 / 1440) % 60);
+  },
+  eorzeaTimeString: function() {
+    var hour = this.eorzeaHour().toString();
+    if (hour.length < 2)
+      hour = '0' + hour;
+    var min = this.eorzeaMinutes().toString();
+    if (min.length < 2)
+      min = '0' + min;
+    return hour + ":" + min;
+  }
+};
+
+
